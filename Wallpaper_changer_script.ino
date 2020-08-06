a1ccf4c4d4c959fd3c39f09b7df02c8a4fc995b4
Gladiator16 <64548701+Gladiator-16@users.noreply.github.com>
2020-08-06 16:31:48 +0530

    Update Wallpaper_changer_script.ino

--- Wallpaper_changer_script.ino
+++ Wallpaper_changer_script.ino
@@ -1,3 +1,10 @@
+//This DigiKeyboard Script takes a screenshot of the desktop, sets it as the wallpaper and hides the desktop icons.
+//Work with Windows XP(?)/Vista(?)/7/8/8.1/10 etc.
+
+//Original Author: CedArctic
+
+//Optimized By: Yosuu
+
 #include "DigiKeyboard.h"
 void setup() {
   //empty
@@ -6,25 +13,47 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(70,226);
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell");
+  DigiKeyboard.print("mspaint");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(800);
+  DigiKeyboard.sendKeyStroke(KEY_V, MOD_CONTROL_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("$client = new-object System.Net.WebClient");
+  DigiKeyboard.sendKeyStroke(KEY_S, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("%USERPROFILE%\\dab.jpg");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.sendKeyStroke(KEY_F, MOD_ALT_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("$client.DownloadFile(\"https://tr3.cbsistatic.com/hub/i/2014/05/15/f8964afd-bd82-4e0e-bcbe-e927363dcdc1/3b858e39e2cf183b878f54cad0073a67/codedoge.jpg\" , \"doge.jpg\")");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.sendKeyStroke(KEY_K);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\doge.jpg\" /f");
+  DigiKeyboard.sendKeyStroke(KEY_F);
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.sendKeyStroke(KEY_F, MOD_ALT_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.sendKeyStroke(KEY_X);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_F10, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_V);
+
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(81);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(81);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(81);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(81);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(81);
+  DigiKeyboard.delay(200);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for(;;){ /*empty*/ }
 }
