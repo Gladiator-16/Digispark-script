5e5ad3b386927f1ddf7f3ecc324b16a1937d8b80
Gladiator16 <64548701+Gladiator-16@users.noreply.github.com>
2020-08-07 09:01:47 +0530

    Update Wallpaper_changer_script.ino

--- Wallpaper_changer_script.ino
+++ Wallpaper_changer_script.ino
@@ -64,5 +64,17 @@ void loop() {
   digitalWrite(0, LOW);
   digitalWrite(1, LOW);
   delay(1000);
+   digitalWrite(0, HIGH);
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  delay(1000);
+   digitalWrite(0, HIGH);
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  delay(1000);
   for(;;){ /*empty*/ }
 }
